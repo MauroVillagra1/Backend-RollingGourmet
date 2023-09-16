2f947b07205373d75eedb874ca90a1a7c6470732
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-16 00:14:57 -0300

    Changes for backend startup

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import orderRouter from "./src/routes/order.route.js";
 import categoryRouter from "./src/routes/category.route.js";
 import userRouter from "./src/routes/user.route.js";
 import "./src/database/database.js"
+import path from "path";
 
 
 const app = express();
@@ -19,6 +20,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(morgan("dev"))
+
+app.use("/api", (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+  });
 
 app.use("/api", productRouter)
 app.use("/api", orderRouter)
