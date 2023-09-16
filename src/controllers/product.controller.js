317ebfd4a9b3f622ff9b50e5b969cc39dd39fdb3
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-15 21:53:24 -0300

    Added Delete Product and Edit Product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -31,3 +31,39 @@ export const createProduct  = async (req, res) => {
         })
     }
 }
+
+export const editProduct  = async (req, res) => {
+    try{
+        console.log(req.params.id)
+        await Product.findByIdAndUpdate(req.params.id,req.body)
+        
+        res.status(200).json({
+            message: "The product was edited successfully"
+        }
+        )
+    }
+    catch(error){
+        console.log(error)
+        res.status(400).json({
+            message: "The product could not be edited"
+        })
+    }
+}
+
+export const deleteProduct  = async (req, res) => {
+    try{
+        console.log(req.params.id)
+        await Product.findByIdAndDelete(req.params.id)
+        
+        res.status(200).json({
+            message: "The product was deleted successfully"
+        }
+        )
+    }
+    catch(error){
+        console.log(error)
+        res.status(400).json({
+            message: "The product could not be deleted"
+        })
+    }
+}
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { createProduct, listProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, editProduct, listProducts } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.route("/products").get(listProducts).post(createProduct)
-
+router.route("/products/:id").put(editProduct).delete(deleteProduct)
 export default router
