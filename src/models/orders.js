eac79e7bbeed88231325374d63cb030ced0c5a82
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-18 20:32:37 -0300

    changes orders

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,31 +1,19 @@
 import mongoose, {Schema} from "mongoose";
 
 const orderSchema = new Schema({
-    UserID:{
+    Order:{
+        type: Array,
+        required: true
+    },
+    State:{
         type: String,
         required: true
     },
-    UserName:{
+    Adress:{
         type: String,
         required: true
     },
-    Email:{
-        type: String,
-        required: true
-    },
-    ProductID: {
-        type: String,
-        require: true
-    },
-    Price:{
-        type: Number,
-        require: true
-    },
-    ProductName: {
-        type: String,
-        required: true
-    },
-    Image: {
+    Date:{
         type: String,
         required: true
     }
