9381c0ba4e7fa4121aa839f6ec88d8c849b9a36d
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-25 13:48:58 -0300

    I added the put route for orders

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { createOrders, deleteOrders, getOrders, listOrders } from "../controllers/order.controller.js";
+import { createOrders, deleteOrders, editOrders, getOrders, listOrders } from "../controllers/order.controller.js";
 
 const router = Router();
 
 router.route("/orders").get(listOrders).post(createOrders)
-router.route("/orders/:id").delete(deleteOrders).get(getOrders)
+router.route("/orders/:id").put(editOrders).delete(deleteOrders).get(getOrders)
 export default router
