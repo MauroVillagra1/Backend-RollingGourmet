6560eb7597cce491920a1fafb2c33ed9bf6c9171
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-27 13:25:55 -0300

    Add Edit user in route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUsers, listUsers, login } from "../controllers/user.controller.js";
+import { createUsers, editUser, listUsers, login } from "../controllers/user.controller.js";
 import { editProduct } from "../controllers/product.controller.js";
 import validarJWT from "../helpers/token-verify.js";
 
@@ -7,6 +7,6 @@ const router = Router();
 
 router.route("/").get([validarJWT],listUsers).post(login)
 router.route("/users").post(createUsers)
-router.route("/user/:id").put([validarJWT],editProduct)
+router.route("/user/:id").put([validarJWT],editUser)
 router.route("/user/admin").post([validarJWT],createUsers)
 export default router
