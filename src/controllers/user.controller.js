af1c82596cb48d01bce1dcefb622523f382c5ade
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-28 21:14:14 -0300

    Solving the error with the email to create a user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ export const listUsers = async (req, res) => {
 export const createUsers = async (req, res) => {
   try {
     const { Email, Password } = req.body;
-    user.State = "User"
+    user.State = "User";
     let user = await User.findOne({ Email });
     if (!user) {
       const newUsers = new User(req.body);
@@ -30,9 +30,12 @@ export const createUsers = async (req, res) => {
         _id: newUsers._id,
       });
     }
-    res.status(400).json({
-      message: "The user exists",
-    });
+    else{
+      res.status(400).json({
+        message: "The user exists",
+      });
+    }
+   
   } catch (error) {
     console.log(error);
     res.status(404).json({
@@ -56,36 +59,31 @@ export const login = async (req, res) => {
         message: "Email or Password Incorrect",
       });
     }
-    if(user.State === "Suspended")
-    {
+    if (user.State === "Suspended") {
       return res.status(400).json({
         message: "User Suspended",
       });
     }
-    if (user.Role === "Admin")
-    {
-      const token = await generarJWT(user._id, user.Name, user.Role)
+    if (user.Role === "Admin") {
+      const token = await generarJWT(user._id, user.Name, user.Role);
       res.status(200).json({
         message: "The user is correct",
         uid: user._id,
         name: user.Name,
         token: token,
         rol: user.Role,
-        img: user.ProfilePicture
-    })
-    }
-    else{
+        img: user.ProfilePicture,
+      });
+    } else {
       res.status(200).json({
         message: "The user is correct",
         uid: user._id,
         name: user.Name,
         token: "",
         rol: user.Role,
-        img: user.ProfilePicture
-
-    })
+        img: user.ProfilePicture,
+      });
     }
-    
   } catch (error) {
     res.status(400).json({
       message: "Email or Password incorrect",
@@ -93,20 +91,18 @@ export const login = async (req, res) => {
   }
 };
 
-export const editUser  = async (req, res) => {
-  try{
-      console.log(req.params.id)
-      await User.findByIdAndUpdate(req.params.id,req.body)
-      
-      res.status(200).json({
-          message: "The User was edited successfully"
-      }
-      )
+export const editUser = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    await User.findByIdAndUpdate(req.params.id, req.body);
+
+    res.status(200).json({
+      message: "The User was edited successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "The User could not be edited",
+    });
   }
-  catch(error){
-      console.log(error)
-      res.status(400).json({
-          message: "The User could not be edited"
-      })
-  }
-}
+};
