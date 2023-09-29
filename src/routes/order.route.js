78787b10e33d97ec4f3f0d3a1fa7d69ca735fa01
jonytellodev <jonathantello.dev@gmail.com>
2023-09-28 23:32:49 -0300

    validations are completed in orders

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -15,9 +15,30 @@ router
   .get(listOrders)
   .post(
     [
-      check("quantity")
+      check("Order")
         .notEmpty()
-        .withMessage("The quantity field is required"),
+        .withMessage(
+          "The order is not correctly created, please check and try again."
+        ),
+      check("IdUser")
+        .notEmpty()
+        .withMessage("Error in IdUser field. Check and try again."),
+      check("NameUser")
+        .notEmpty()
+        .withMessage(
+          "There is an error in the NameUser field or it is empty. Please check."
+        ),
+      check("State")
+        .notEmpty()
+        .withMessage("State field not valid. Please check")
+        .isString()
+        .withMessage("the 'State' value received must be a string"),
+        check("Adress")
+        .notEmpty()
+        .withMessage("Address field error. Please check."),
+      check("Date")
+        .notEmpty()
+        .withMessage("Error in the Date field. Check and try again."),
     ],
     createOrders
   );
