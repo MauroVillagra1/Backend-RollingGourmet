e6c9afa52e92f910d2d70a232e38fb183de9ac87
Mauro Villagra <maurovillagra257@gmail.com>
2023-10-03 10:19:11 -0300

    delete coments

--- src/helpers/token-verify.js
+++ src/helpers/token-verify.js
@@ -2,15 +2,12 @@ import jwt from 'jsonwebtoken'
 //esto es un middleware personalizado
 
 const validarJWT = (req, res, next)=>{
-    //recibir el token
     const token = req.header('x-token');
     if(!token){
-        //401 error en la autenticacion
         return res.status(401).json({
             mensaje: 'No hay token en la peticion'
         })
     }
-    // si el token existe
     try{
         const payload =  jwt.verify(token, process.env.SECRET_JWT)
         req.id = payload.uid;
