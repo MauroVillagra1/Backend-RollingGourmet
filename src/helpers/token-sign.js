701b4df30c227bcd4d8674ed7fb0cb6c5cba53e7
Mauro Villagra <maurovillagra257@gmail.com>
2023-10-03 10:18:43 -0300

    delete coments

--- src/helpers/token-sign.js
+++ src/helpers/token-sign.js
@@ -3,9 +3,7 @@ import "dotenv/config"
 
 const generarJWT = (uid, name, role )=>{
     return new Promise( (resolve, reject)=>{
-        //agregar los datos al payload
         const payload = {uid, name, role};
-        //aqui firmamos el token
         jwt.sign(payload,process.env.SECRET_JWT,{
             expiresIn: '3h'
         },(err, token)=>{
@@ -13,7 +11,6 @@ const generarJWT = (uid, name, role )=>{
                 console.log(err);
                 reject('token could not be found')
             }
-            //si esta todo correcto
             resolve(token);
         })
     })
